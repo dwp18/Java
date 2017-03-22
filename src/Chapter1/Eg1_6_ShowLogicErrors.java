322b39b333fda523dcc9e37638031ce27e0c5526
Dimas Wahyu <dimaswahyu18@gmail.com>
2017-03-22 12:27:23 +0700

    [Eg1.6] Logic Errors, Fix Error - Adding .0 for decimal division

--- src/Chapter1/Eg1_6_ShowLogicErrors.java
+++ src/Chapter1/Eg1_6_ShowLogicErrors.java
@@ -6,6 +6,6 @@ package Chapter1;
 public class Eg1_6_ShowLogicErrors {
     public static void main(String[] agrs){
         System.out.println("Celcius 35 is Fahrenheit degree ");
-        System.out.println((9 / 5) * 35 + 32);
+        System.out.println((9.0 / 5) * 35 + 32);
     }
 }
