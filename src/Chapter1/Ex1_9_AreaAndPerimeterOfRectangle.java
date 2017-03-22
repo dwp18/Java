f618c9570330e7ce663a58db9228901ba0ed580c
Dimas Wahyu <dimaswahyu18@gmail.com>
2017-03-22 14:32:02 +0700

    [Ex1.9] Calculate Area of a Rectangle

---
+++ src/Chapter1/Ex1_9_AreaAndPerimeterOfRectangle.java
@@ -0,0 +1,17 @@
+package Chapter1;
+
+/**
+ * Created by Wahyu on 3/22/2017.
+ */
+public class Ex1_9_AreaAndPerimeterOfRectangle {
+    public static void main(String[] args){
+        System.out.println("Known    : Width  = 4.5");
+        System.out.println("           Height = 7.9");
+        System.out.println("Question : How broad the area of a rectangle is?");
+        System.out.println("");
+        System.out.println("Area     = width x height");
+        System.out.println("         = 4.5 * 7.9");
+        System.out.print("         = ");
+        System.out.println(4.5 * 7.9);
+    }
+}
