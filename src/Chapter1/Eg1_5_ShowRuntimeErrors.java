43bd39dd486ebb75beaa68ed4120cad5ed901d6b
Dimas Wahyu <dimaswahyu18@gmail.com>
2017-03-22 12:09:07 +0700

    Adding Chapter 1 Example 5

---
+++ src/Chapter1/Eg1_5_ShowRuntimeErrors.java
@@ -0,0 +1,10 @@
+package Chapter1;
+
+/**
+ * Created by Wahyu on 3/22/2017.
+ */
+public class Eg1_5_ShowRuntimeErrors {
+    public static void main(String[] args){
+        System.out.println(1 / 0);
+    }
+}
