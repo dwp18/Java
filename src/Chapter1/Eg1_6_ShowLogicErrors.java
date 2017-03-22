9ac34200cd2348e752ec6f84579b79138efcc534
Dimas Wahyu <dimaswahyu18@gmail.com>
2017-03-22 12:24:33 +0700

    [Eg1.6] Logic Errors

---
+++ src/Chapter1/Eg1_6_ShowLogicErrors.java
@@ -0,0 +1,11 @@
+package Chapter1;
+
+/**
+ * Created by Wahyu on 3/22/2017.
+ */
+public class Eg1_6_ShowLogicErrors {
+    public static void main(String[] agrs){
+        System.out.println("Celcius 35 is Fahrenheit degree ");
+        System.out.println((9 / 5) * 35 + 32);
+    }
+}
