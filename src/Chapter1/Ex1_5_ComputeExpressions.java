c8da8d4d75c835edaf91a958b33d973e22346feb
Dimas Wahyu <dimaswahyu18@gmail.com>
2017-03-22 13:32:30 +0700

    [Ex1.5] Compute Expression (Parentheses in Calculation)

---
+++ src/Chapter1/Ex1_5_ComputeExpressions.java
@@ -0,0 +1,13 @@
+package Chapter1;
+
+/**
+ * Created by Wahyu on 3/22/2017.
+ */
+public class Ex1_5_ComputeExpressions {
+    public static void main(String[] args){
+        System.out.println("9.5 x 4.5 - 2.5 x 3");
+        System.out.print("------------------- = ");
+        System.out.println(((9.5 * 4.5) - (2.5 * 3)) / (45.5 - 3.5));
+        System.out.println("\t45.5 - 3.5");
+    }
+}
