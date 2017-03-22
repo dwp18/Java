84673d34ff0994bf0de8be178e34cd6c4f4be216
Dimas Wahyu <dimaswahyu18@gmail.com>
2017-03-22 12:48:11 +0700

    [Ex1.1] Display Three Messages

---
+++ src/Chapter1/Ex1_1_DisplayThreeMessages.java
@@ -0,0 +1,12 @@
+package Chapter1;
+
+/**
+ * Created by Wahyu on 3/22/2017.
+ */
+public class Ex1_1_DisplayThreeMessages {
+    public static void main(String[] args){
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Computer Science");
+        System.out.println("Programming is fun");
+    }
+}
