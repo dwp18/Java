6b3df630a58715ff8d3f106d9182caa6a989a9c1
Dimas Wahyu <dimaswahyu18@gmail.com>
2017-03-22 12:00:20 +0700

    Adding Chapter 1 Example 4

---
+++ src/Chapter1/Eg1_4_ShowSyntaxErrors.java
@@ -0,0 +1,10 @@
+package Chapter1;
+
+/**
+ * Created by Wahyu on 3/22/2017.
+ */
+public class Eg1_4_ShowSyntaxErrors {
+    public static void main(String[] agrs){
+        System.out.println("Welcome to Java");
+    }
+}
