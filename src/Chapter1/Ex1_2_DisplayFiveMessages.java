863f5e985d0285562487d37062f291bf868e573b
Dimas Wahyu <dimaswahyu18@gmail.com>
2017-03-22 12:51:27 +0700

    [Ex1.2] Display Five Same Messages

---
+++ src/Chapter1/Ex1_2_DisplayFiveMessages.java
@@ -0,0 +1,14 @@
+package Chapter1;
+
+/**
+ * Created by Wahyu on 3/22/2017.
+ */
+public class Ex1_2_DisplayFiveMessages {
+    public static void main(String[] args){
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+    }
+}
