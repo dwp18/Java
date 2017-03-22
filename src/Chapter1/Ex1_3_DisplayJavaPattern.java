bae4e32f98df0326781c66617823726d224d857c
Dimas Wahyu <dimaswahyu18@gmail.com>
2017-03-22 13:00:52 +0700

    [Ex1.3] Display JAVA in Pattern

---
+++ src/Chapter1/Ex1_3_DisplayJavaPattern.java
@@ -0,0 +1,13 @@
+package Chapter1;
+
+/**
+ * Created by Wahyu on 3/22/2017.
+ */
+public class Ex1_3_DisplayJavaPattern {
+    public static void main(String[] args){
+        System.out.println("    J     A     V     V     A");
+        System.out.println("    J    A A     V   V     A A");
+        System.out.println("J   J   AAAAA     V V     AAAAA");
+        System.out.println(" J J   A     A     V     A     A");
+    }
+}
