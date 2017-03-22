ad65bcacf155f50e2998a15f0bf5e1dcd09857db
Dimas Wahyu <dimaswahyu18@gmail.com>
2017-03-22 11:19:34 +0700

    Adding Chapter 1 Example 3

---
+++ src/Chapter1/Eg1_3_ComputerExpression.java
@@ -0,0 +1,10 @@
+package Chapter1;
+
+/**
+ * Created by Wahyu on 3/22/2017.
+ */
+public class Eg1_3_ComputerExpression {
+    public static void main(String[] agrs){
+        System.out.println((10.5 + 2 * 3) / (45 - 3.5));
+    }
+}
