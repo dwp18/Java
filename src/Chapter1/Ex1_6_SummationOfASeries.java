c202c9ecece4682f436cd137392ffca6d03fe729
Dimas Wahyu <dimaswahyu18@gmail.com>
2017-03-22 14:13:18 +0700

    [Ex1.6] Summation of Series (Increment Numbers)

---
+++ src/Chapter1/Ex1_6_SummationOfASeries.java
@@ -0,0 +1,11 @@
+package Chapter1;
+
+/**
+ * Created by Wahyu on 3/22/2017.
+ */
+public class Ex1_6_SummationOfASeries {
+    public static void main(String[] args){
+        System.out.print("1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 = ");
+        System.out.print(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9);
+    }
+}
