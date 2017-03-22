6db960543f79398c608f3c24d4c767934d0d3bbe
Dimas Wahyu <dimaswahyu18@gmail.com>
2017-03-22 10:36:30 +0700

    Adding Chapter 1 Example 1

---
+++ src/Chapter1/Eg1_1_Welcome.java
@@ -0,0 +1,11 @@
+package Chapter1;
+
+/**
+ * Created by Wahyu on 3/20/2017.
+ */
+public class Eg1_1_Welcome { //Class
+    public static void  main(String[] args){ //Main Method
+        //syntax for print
+      System.out.println("Welcome To Java!"); //Statement
+    }
+}
