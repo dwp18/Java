85720e7bff3771d92fa85ef51861e2a74cf863f9
Dimas Wahyu <dimaswahyu18@gmail.com>
2017-03-22 11:02:51 +0700

    Adding Chapter 1 Example 2

---
+++ src/Chapter1/Eg1_2_WelcomeWithThreeMessages.java
@@ -0,0 +1,12 @@
+package Chapter1;
+
+/**
+ * Created by Wahyu on 3/22/2017.
+ */
+public class Eg1_2_WelcomeWithThreeMessages {
+    public static void main(String[] args){
+        System.out.println("Programming is fun!");
+        System.out.println("Fundamental First");
+        System.out.println("Problem Driven");
+    }
+}
