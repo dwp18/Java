3fcc97e179bfb3a57b92a8475f7ed623bbc7dc2a
Dimas Wahyu <dimaswahyu18@gmail.com>
2017-03-22 13:18:35 +0700

    [Ex1.4] Print Table contains Power calculation (manual)

---
+++ src/Chapter1/Ex1_4_PrintATable.java
@@ -0,0 +1,35 @@
+package Chapter1;
+
+/**
+ * Created by Wahyu on 3/22/2017.
+ */
+public class Ex1_4_PrintATable {
+    public static void main(String[] args){
+        System.out.println("a\t\ta^2\t\ta^3");
+
+        System.out.print(1);
+        System.out.print("\t\t");
+        System.out.print(1*1);
+        System.out.print("\t\t");
+        System.out.println(1*1*1);
+
+        System.out.print(2);
+        System.out.print("\t\t");
+        System.out.print(2*2);
+        System.out.print("\t\t");
+        System.out.println(2*2*2);
+
+        System.out.print(3);
+        System.out.print("\t\t");
+        System.out.print(3*3);
+        System.out.print("\t\t");
+        System.out.println(3*3*3);
+
+        System.out.print(4);
+        System.out.print("\t\t");
+        System.out.print(4*4);
+        System.out.print("\t\t");
+        System.out.println(4*4*4);
+        }
+    }
+
