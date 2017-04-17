d2985d4e28356510c823aef35d1f5e0a221b7d9b
Dimas Wahyu <dimaswahyu18@gmail.com>
2017-04-17 13:27:13 +0700

    [Eg3.1] Addition Quiz [Boolean Operator - True/False]

---
+++ src/Chapter3/AdditionQuiz.java
@@ -0,0 +1,21 @@
+package Chapter3;
+
+import java.util.Scanner;
+
+/**
+ * Created by Wahyu on 4/17/2017.
+ */
+public class AdditionQuiz {
+    public static void main (String[] args){
+        int number1 = (int)(System.currentTimeMillis() % 10);
+        int number2 = (int)(System.currentTimeMillis() /7 % 10);
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("What is " + number1 + " + " + number2 + "? ");
+        int result = input.nextInt();
+
+        System.out.print(number1 + " + " + number2 + " = " + result + " is " + (number1 + number2 == result));
+
+    }
+}
