206c10b4ffce2d77c37d4214eae64cd5da39c908
Dimas Wahyu <dimaswahyu18@gmail.com>
2017-04-20 10:03:46 +0700

    [Ex3.11] Find number of days in month [Fix - Conditional Expressions]

--- src/Chapter3/Ex3_11_FindNumberOfDaysInMonth.java
+++ src/Chapter3/Ex3_11_FindNumberOfDaysInMonth.java
@@ -34,10 +34,8 @@ public class Ex3_11_FindNumberOfDaysInMonth {
 
         if (month == 1 || month == 3 || month == 5  || month == 7  || month == 8  || month == 10  || month == 12) {
             totalDays = 31;
-        } else if (month == 2 && leapYear){
-            totalDays = 29;
         } else if (month == 2){
-            totalDays = 28;
+            totalDays = (leapYear) ? 29 : 28;
         } else {
             totalDays = 30;
         }
